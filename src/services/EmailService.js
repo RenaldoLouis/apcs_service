e96057ebdb3119537114d61d88805c54f9b67eb0
RenaldoLouis <renald@10xers.co>
2024-08-13 23:06:19 +0700

    send email success

--- src/services/EmailService.js
+++ src/services/EmailService.js
@@ -2,14 +2,44 @@
 const db = require('../repositories/PaymentRepository.js');
 const databaseUtil = require('../utils/DatabaseUtil.js');
 const { logger } = require('../utils/Logger');
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: "renaldolouis555@gmail.com",
+        pass: "obim loxb nwll dukt",
+    },
+});
+
+const mailOptions = {
+    from: "renaldolouis555@gmail.com",
+    to: "vayneaurelius5@gmail.com",
+    subject: "Hello from Nodemailer",
+    text: "This is a test email sent using Nodemailer.",
+};
 
 async function sendEmail(req) {
     const body = req.body;
+
+    // Wrap the sendMail function in a Promise
+    const sendMailPromise = new Promise((resolve, reject) => {
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                reject({ message: "email Fail" });
+            } else {
+                resolve({ message: "email" });
+            }
+        });
+    });
+
     try {
-        // return await databaseUtil.executeDatabaseOperation(db.createPayment, body);
-        return {
-            message: "email"
-        };
+        // await the result of the sendMailPromise
+        const result = await sendMailPromise;
+        return result;
     } catch (error) {
         throw error;
     }
