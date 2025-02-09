d7981872952a5e4b7b13407ae81fda719c6b7f2d
Renaldo Louis <renaldo.louis@commsult.id>
2025-02-10 06:46:22 +0700

    update returned objet from getVideos

--- src/repositories/FirebaseRepository.js
+++ src/repositories/FirebaseRepository.js
@@ -49,11 +49,35 @@ const getVideos = async (params, callback) => {
 
     const response = await s3.send(command);
 
-    // Extract folder names from CommonPrefixes
-    const files = response.Contents?.map(file => `https://${bucketName}.s3.${process.env.AWS_REGION}.amazonaws.com/${file.Key}`) || [];
-    files.shift();
+    let returnObject = []
 
-    return callback(null, files);
+    response.Contents?.shift()
+    response.Contents?.forEach((eachVideo) => {
+        const fileName = eachVideo.Key
+        let cleanPath = fileName.replace(/^videos\//, "");
+
+        const link = `https://${bucketName}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`
+
+        // Remove the extension
+        let withoutExtension = cleanPath.replace(/\.[^/.]+$/, "");
+
+        // Extract the number inside parentheses
+        let match = withoutExtension.match(/\((\d+)\)$/);
+        let order = match ? parseInt(match[1]) : 1;
+
+        // Remove the number in parentheses from the name
+        let cleanName = withoutExtension.replace(/\(\d+\)$/, "");
+
+        let tempObject = {
+            name: cleanName,
+            order: order,
+            video: link
+        }
+
+        returnObject.push(tempObject)
+    })
+
+    return callback(null, returnObject);
 };
 
 module.exports = {
