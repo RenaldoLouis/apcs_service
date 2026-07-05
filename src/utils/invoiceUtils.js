36225685e0765c98bf283cc217ca45fea6353593
Renaldo Louis <renaldo.louis@commsult.id>
2026-07-05 12:06:32 +0700

    update default rate

--- src/utils/invoiceUtils.js
+++ src/utils/invoiceUtils.js
@@ -10,7 +10,7 @@
  * when the Firestore value is unavailable.
  */
 
-const DEFAULT_USD_TO_IDR_RATE = 17800;
+const DEFAULT_USD_TO_IDR_RATE = 17900;
 
 /**
  * Converts a single invoice item from the frontend format into the
